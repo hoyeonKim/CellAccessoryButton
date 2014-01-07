1b0e8d5e68084eaa5797f91f7da4f3e035ed3d65
hoyeonKim <roadmaker@korea.ac.kr>
2014-01-07 21:50:56 +0900

    CellAccessory
    완료했습니

--- p174/ViewController.m
+++ p174/ViewController.m
@@ -7,17 +7,46 @@
 //
 
 #import "ViewController.h"
+#define CELL_ID @"CELL_ID"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDataSource,UITabBarDelegate>
 
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    NSArray *data;
+}
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    NSLog(@"Select Row: %d",indexPath.row);
+}
 
+- (void)tableView:(UITableView *)tableView accessoryButtonTappedForRowWithIndexPath:(NSIndexPath *)indexPath{
+    NSLog(@"Accessory Button tapped : %d",indexPath.row);
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [data count];
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CELL_ID];
+    
+    if(0== indexPath.row)
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    else if (1==indexPath.row)
+        cell.accessoryType = UITableViewCellAccessoryDetailDisclosureButton;
+    else if (2==indexPath.row)
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    
+    cell.textLabel.text = data[indexPath.row];
+    
+    return cell;
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    data = @[@"a",@"b",@"c",@"d",@"e",@"f",@"g",@"h",@"i"];
 }
 
 - (void)didReceiveMemoryWarning
